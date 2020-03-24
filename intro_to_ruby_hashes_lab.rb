2ef96ab0a486d7aa61b990c6a12c603eda3a2882
Jennifer Paquette <jennidemuir@gmail.com>
2020-03-24 00:08:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -36,9 +36,9 @@ def update_counting_hash(hash, key)
   # if the provided key is present, increment its value by 1
 hash = Hash.new
   if hash[key]
-   hash[key]=1 
+   hash[key]+=1 
   else
-    hash[key]+=1 
+    hash[key]=1 
 end
 hash
 end
